6e7935970f96b8d082f63cf9c400d2c4d1686c8a
shubh51 <46393698+shubh51@users.noreply.github.com>
2022-09-13 01:36:28 +0530

    Update 1_Maps.cpp

--- STL/1_Maps.cpp
+++ STL/1_Maps.cpp
@@ -1,1 +1,31 @@
 
+class Solution
+{
+    /*
+    Twice Counter GFG problem
+    Solved Using Unurdered_map
+    
+    Given a list of N words.
+    Count the number of words that appear exactly twice in the list.
+    */
+    public:
+        int countWords(string list[], int n)
+        {
+            // map for storing words with their frequency 
+           unordered_map<string, int>wordFreq; 
+           for(int i =0; i < n; i++)
+                wordFreq[list[i]]++;
+            int counter = 0; 
+            for(auto x : wordFreq)
+            {
+                // checking whether particular word is having apperance twice or not 
+                if(x.second == 2)
+                    counter++; 
+            }
+            return counter;
+}
+
+  
+    
+
+};
