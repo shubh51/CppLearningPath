cbf7838c688e17fcb881250603e0eb8dfc3ffb0f
shubh51 <46393698+shubh51@users.noreply.github.com>
2022-09-13 16:43:35 +0530

    Create 7_Pair_STL_GFG.cpp

---
+++ STL/7_Pair_STL_GFG.cpp
@@ -0,0 +1,38 @@
+
+/* Inserts a pair  <string,   pair<x, y> > >    into the vector A */
+void add_pair(vector<pair<string,pair<int,int> > > &A, string str, int x, int y)
+{
+     // Your code here
+     A.push_back({str, {x, y}});
+}
+
+/* Returns the size of the vector A */
+int get_size(vector<pair<string,pair<int,int> > > &A)
+{
+     // Your code here
+     return A.size();
+}
+
+/* Prints space separated values of vector A */
+void print_values(vector<pair<string,pair<int,int> > > &A)
+{
+     // Your code here
+     for(auto itr: A)
+     {
+         cout<<itr.first<<" "<<itr.second.first<<" "<<itr.second.second<<" ";
+     }
+}
+
+/* Sorts the vector A based on value x and y*/
+void sort_pair(vector<pair<string,pair<int,int> > > &A)
+{
+     // Your code here
+      sort(
+            A.begin(), 
+            A.end(), 
+            [](pair<string,pair<int,int>> &p1, pair<string,pair<int,int>> &p2)
+            {
+                return p1.second < p2.second;
+            }
+        );
+}
